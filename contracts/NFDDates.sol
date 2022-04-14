71e6603ae5e95b92a92ac058e0830308f59c715d
jeff mascarenhas <jeftar.carvalho@tc.com.br>
2022-04-13 21:17:46 -0300

    feat: contract creation

--- contracts/NFDDates.sol
+++ contracts/NFDDates.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
@@ -12,186 +13,141 @@ contract NFDDates is
     Initializable,
     UUPSUpgradeable,
     OwnableUpgradeable,
-    PausableUpgradeable
+    PausableUpgradeable,
+    ERC721Upgradeable
 {
-    string private _name;
-    string private _symbol;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
 
-    // Mapping from token ID to owner address
-    mapping(uint256 => address) private _owners;
-    // Mapping owner address to token count
-    mapping(address => uint256) private _balances;
-    // Mapping from token ID to approved address
-    mapping(uint256 => address) private _tokenApprovals;
-    // Mapping from owner to operator approvals
-    mapping(address => mapping(address => bool)) private _operatorApprovals;
+    mapping(uint256 => MarketItem) private idToMarketItem;
 
-    event Transfer(address indexed from, address indexed to, uint256 tokenId);
-    event Approval(
-        address indexed owner,
-        address indexed approved,
-        uint256 indexed tokenId
-    );
-    event ApprovalForAll(
-        address indexed owner,
-        address indexed operator,
-        bool approved
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool forSale;
+    }
+
+    event MarketItemCreated(
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool forSale
     );
 
-    function initialize(string memory _initName, string memory _initSymbol)
+    function initialize(string memory initName_, string memory initSymbol_)
         public
         initializer
     {
-        _name = _initName;
-        _symbol = _initSymbol;
+        __ERC721_init(initName_, initSymbol_);
         __Ownable_init();
     }
 
     function _authorizeUpgrade(address) internal override onlyOwner {}
 
-    function name() public view returns (string memory) {
-        return _name;
-    }
-
-    function symbol() public view returns (string memory) {
-        return _symbol;
-    }
-
     function totalSupply() public view returns (uint256) {
         return _tokenIds.current();
     }
 
-    function balanceOf(address owner) public view virtual returns (uint256) {
-        require(owner != address(0), "NFDDates: balance for zero address");
-        return _balances[owner];
+    function createMarketItem(uint256 tokenId_, uint256 price_) private {
+        idToMarketItem[tokenId_] = MarketItem(
+            tokenId_,
+            payable(msg.sender),
+            payable(msg.sender),
+            price_,
+            false
+        );
+        emit MarketItemCreated(
+            tokenId_,
+            msg.sender,
+            payable(msg.sender),
+            price_,
+            false
+        );
     }
 
-    function ownerOf(uint256 tokenId) public view virtual returns (address) {
-        address owner = _owners[tokenId];
-        require(owner != address(0), "NFDDates: owner nonexistent token");
-        return owner;
+    function createToken(uint256 price_) private returns (uint256) {
+        _tokenIds.increment();
+        uint256 tokenId = _tokenIds.current();
+        _safeMint(msg.sender, tokenId);
+        createMarketItem(tokenId, price_);
+        return tokenId;
     }
 
-    function buy(address _to)
+    function createMarketSale(uint256 price_) public payable whenNotPaused {
+        require(
+            price_ > 0 && msg.value == price_,
+            "Price must be at least 1 wei"
+        );
+        createToken(price_);
+        _itemsSold.increment();
+    }
+
+    function createResellMarketSale(uint256 percentage_, uint256 tokenId_)
         public
         payable
         whenNotPaused
-        onlyOwner
-        returns (uint256, address)
     {
-        _tokenIds.increment();
-        uint256 newToken = _tokenIds.current();
-        _mint(_to, newToken);
-
-        return (newToken, _to);
-    }
-
-    function _mint(address to, uint256 tokenId) internal virtual {
-        console.log("Address zero: ", owner());
-        require(to != address(0), "NFDDates: mint to the zero address");
-        require(!_exists(tokenId), "NFDDates: token already minted");
-
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-        emit Transfer(address(0), to, tokenId);
-    }
-
-    function _isApprovedOrOwner(address spender, uint256 tokenId)
-        internal
-        view
-        virtual
-        returns (bool)
-    {
+        uint256 itemPrice = idToMarketItem[tokenId_].price;
         require(
-            _exists(tokenId),
-            "NFDDates: operator query for nonexistent token"
+            msg.value > 0 && msg.value >= itemPrice,
+            "NFDDates: Price is not correct"
         );
-        address owner = NFDDates.ownerOf(tokenId);
-        return (spender == owner ||
-            getApproved(tokenId) == spender ||
-            isApprovedForAll(owner, spender));
+        uint256 platformValue = (msg.value * percentage_) / 100;
+        uint256 sellValue = msg.value - platformValue;
+
+        address payable seller = idToMarketItem[tokenId_].seller;
+
+        console.log("oldSeller = ", seller);
+
+        _transfer(address(this), msg.sender, tokenId_);
+        idToMarketItem[tokenId_].price = msg.value;
+        idToMarketItem[tokenId_].forSale = false;
+        idToMarketItem[tokenId_].seller = payable(msg.sender);
+        idToMarketItem[tokenId_].owner = payable(msg.sender);
+        emit MarketItemCreated(
+            tokenId_,
+            payable(msg.sender),
+            payable(msg.sender),
+            idToMarketItem[tokenId_].price,
+            false
+        );
+        console.log("newSeller = ", idToMarketItem[tokenId_].seller);
+        payable(seller).transfer(sellValue);
     }
 
-    function getApproved(uint256 tokenId)
+    function resellToken(uint256 price_, uint256 tokenId_)
         public
-        view
-        virtual
-        returns (address)
+        payable
+        whenNotPaused
     {
         require(
-            _exists(tokenId),
-            "NFDDates: approved query for nonexistent token"
+            idToMarketItem[tokenId_].seller == msg.sender,
+            "NFDDates: Only owner of token"
         );
 
-        return _tokenApprovals[tokenId];
-    }
+        idToMarketItem[tokenId_].forSale = true;
+        idToMarketItem[tokenId_].price = price_;
+        idToMarketItem[tokenId_].seller = payable(msg.sender);
+        idToMarketItem[tokenId_].owner = payable(address(this));
 
-    function _approve(address to, uint256 tokenId) internal virtual {
-        _tokenApprovals[tokenId] = to;
-        emit Approval(NFDDates.ownerOf(tokenId), to, tokenId);
-    }
+        _itemsSold.decrement();
 
-    function isApprovedForAll(address owner, address operator)
-        public
-        view
-        virtual
-        returns (bool)
-    {
-        return _operatorApprovals[owner][operator];
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual whenNotPaused {
-        //solhint-disable-next-line max-line-length
-        require(
-            _isApprovedOrOwner(_msgSender(), tokenId),
-            "NFDDates: transfer caller is not owner nor approved"
+        emit MarketItemCreated(
+            tokenId_,
+            payable(msg.sender),
+            payable(address(this)),
+            price_,
+            true
         );
 
-        _transfer(from, to, tokenId);
+        _transfer(msg.sender, address(this), tokenId_);
     }
 
-    function _transfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal virtual {
-        require(
-            NFDDates.ownerOf(tokenId) == from,
-            "NFDDates: transfer from incorrect owner"
-        );
-        require(to != address(0), "NFDDates: transfer to the zero address");
-
-        // Clear approvals from the previous owner
-        _approve(address(0), tokenId);
-
-        _balances[from] -= 1;
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-
-        emit Transfer(from, to, tokenId);
-    }
-
-    function _setApprovalForAll(
-        address owner,
-        address operator,
-        bool approved
-    ) internal virtual {
-        require(owner != operator, "ERC721: approve to caller");
-        _operatorApprovals[owner][operator] = approved;
-        emit ApprovalForAll(owner, operator, approved);
-    }
-
-    function _exists(uint256 tokenId) internal view virtual returns (bool) {
-        return _owners[tokenId] != address(0);
-    }
-
-    function setPause() public virtual onlyOwner {
+    function setPause() public onlyOwner {
         _pause();
     }
 
@@ -199,7 +155,15 @@ contract NFDDates is
         return address(this).balance;
     }
 
-    function setUnPause() public virtual onlyOwner {
+    function setUnPause() public onlyOwner {
         _unpause();
+    }
+
+    function withdraw() public payable onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "NFDDates: No ether left to withdraw");
+
+        (bool success, ) = (msg.sender).call{value: balance}("");
+        require(success, "NFDDates: Transfer failed.");
     }
 }
