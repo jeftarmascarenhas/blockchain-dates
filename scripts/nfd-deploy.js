36f00bf87b726d3afa2ac91f963c66bfec996a44
jeff mascarenhas <>
2022-04-16 22:30:09 -0300

    feat: config and contract updated

--- scripts/nfd-deploy.js
+++ scripts/nfd-deploy.js
@@ -1,14 +1,37 @@
-const { ethers, upgrades } = require("hardhat");
+/* eslint-disable node/no-path-concat */
+/* eslint-disable no-process-exit */
+const { ethers, upgrades, artifacts } = require("hardhat");
 
 async function main() {
   const NFDDates = await ethers.getContractFactory("NFDDates");
-  console.log("Deploying Pizza...");
+  console.log("Deploying NFDDates...");
 
-  const nfdDates = await upgrades.deployProxy(NFDDates, ["NAMED"], {
+  const nfdDates = await upgrades.deployProxy(NFDDates, [], {
     initializer: "initialize",
   });
 
   console.log("NFDDates deployed to: ", nfdDates.address);
+  buildFiles(nfdDates);
+}
+
+function buildFiles(nfdDates) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../build/contracts";
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + "/NFDDates-address.json",
+    JSON.stringify({ NFDDates: nfdDates.address }, undefined, 2)
+  );
+
+  const NFDDatesArtifact = artifacts.readArtifactSync("NFDDates");
+
+  fs.writeFileSync(
+    contractsDir + "/NFDDates.json",
+    JSON.stringify(NFDDatesArtifact, null, 2)
+  );
 }
 
 main()
